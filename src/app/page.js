f9696cfe2877f17afa5bd43d138888c2382ba8fd
umayer10-AI <mdumayer10@gmail.com>
2026-05-21 11:49:54 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import AddUser from '@/component/AddUser';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       Hello Umayer
+      <AddUser></AddUser>
     </div>
   );
 };
---
+++ src/component/AddUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h2>USer List:</h2>
+            <input type="text" placeholder='Add user' className='border' />
+            <button>Add</button>
+        </div>
+    );
+};
+
+export default AddUser;
